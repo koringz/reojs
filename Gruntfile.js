86a64fbdfb3133123326b2dd04c7c350bb8b706f
koringz <ok234@foxmail.com>
2017-03-30 23:33:24 +0800

    up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,124 @@
-'use strict';
-
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      all: ['test/{grunt,tasks,util}/**/*.js'],
-      tap: {
-        src: '<%= nodeunit.all %>',
+  //package.json文件  
+  var definePKg = grunt.file.readJSON('package.json'); 
+
+  //concat合并js文件  
+  var defineConcat = {
+      options : {
+        separetor :';'
+      },
+      dist : {
+        src : ['app/src/**.js'],
+        dest : 'app/build/<%= pkg.name %>.js'
+      },
+  };
+
+  // 压缩文件，左边为输出压缩文件，右边为需要压缩的文件
+  var defineUglify = {
+    
+    my_target : {
+        options : {
+          mangle : {
+            execpt : ['']
+          }
+        },
+        files : {
+          'dest/**.js' : ['dist/**.js'],
+        }
+    }
+
+  };
+
+  // js规范验证
+  var defineJshint = {
+     options : {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+     },
+     files : ['Gruntfile.js', '/src/**/*.js'],
+  };
+
+  // 监听文件变化
+  var defineWatch = {
+      scripts :{
+        files: ['lib/*.js'],
+        tasks: ['jshint'],
         options: {
-          reporter: 'tap',
-          reporterOutput: 'tests.tap'
+          spawn: false,
+          livereload: 0123
+        },
+      },
+      css : {
+        files : ['assets/**.css'],
+        tasks : ['css'],
+        options:{
+          livereload : 1234
+        }
+      },
+      html : {
+        files :['src/**.html'],
+        options:{
+          livereload : 2345
+        }
+      },
+      jshint : {
+        files : ['assets/**.js']
+      }
+  };
+
+  // 服务器连接服务,依赖注入进html,如:jq sea angular
+  var defineWiredep = {
+      target: {
+        src: 'src/**.html' // point to your HTML file.
+      }
+  };
+
+  // 端口服务
+  var defineConnect = {
+      server : {
+        options : {
+            part : 9000,
+            keepalive:true,
+            open: true,
+            hostname : 'localhost',
+            base : '',
         }
       }
-    },
-    jshint: {
-      gruntfile_tasks: ['Gruntfile.js', 'internal-tasks/*.js'],
-      libs_n_tests: ['lib/**/*.js', '<%= nodeunit.all %>'],
-      subgrunt: ['<%= subgrunt.all %>'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    jscs: {
-      src: [
-        'lib/**/*.js',
-        'internal-tasks/**/*.js',
-        'test/**/*.js',
-        '!test/fixtures/**/*.js'
-      ]
-    },
-    watch: {
-      gruntfile_tasks: {
-        files: ['<%= jshint.gruntfile_tasks %>'],
-        tasks: ['jshint:gruntfile_tasks']
-      },
-      libs_n_tests: {
-        files: ['<%= jshint.libs_n_tests %>'],
-        tasks: ['jshint:libs_n_tests', 'nodeunit']
-      },
-      subgrunt: {
-        files: ['<%= subgrunt.all %>'],
-        tasks: ['jshint:subgrunt', 'subgrunt']
-      }
-    },
-    subgrunt: {
-      all: ['test/gruntfile/*.js']
-    },
-  });
+  };
 
-  // These plugins provide necessary tasks.
+  grunt.initConfig({
+
+    pkg: definePKg,
+
+    concat: defineConcat,
+
+    uglify: defineUglify,
+
+    jshint : defineJshint,
+
+    wiredep: defineWiredep, 
+       
+    connect: defineConnect,
+
+    watch : defineWatch
+
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscs');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Some internal tasks. Maybe someday these will be released.
-  grunt.loadTasks('internal-tasks');
-
-  // "npm test" runs these tasks
-  grunt.registerTask('test', '', function(reporter) {
-    grunt.task.run(['jshint', 'jscs', 'nodeunit:' + (reporter || 'all'), 'subgrunt']);
-  });
-
-  // Default task.
-  grunt.registerTask('default', ['test']);
-
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('start', ['concat','uglify','jshint','wiredep','watch','connect']);
+  grunt.registerTask('default', ['start']);
 };
+
+
+// http://blog.csdn.net/ch717828/article/details/50339087
